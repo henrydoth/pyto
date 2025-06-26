dc4c19dd7f1b6f25b7400e2f739a287e4491d686
henry <liem20k@gmail.com>
2025-06-26 10:33:00 +0700

    làm tròn thập phân

--- thong_ke_co_ban_wc.py
+++ thong_ke_co_ban_wc.py
@@ -1,4 +1,3 @@
-
 import statistics
 
 # Dữ liệu mẫu
@@ -6,11 +5,19 @@ data = [12, 15, 14, 10, 60
 , 15, 18, 20, 15, 14]
 
 # Thống kê cơ bản
-mean = statistics.mean(data)
-median = statistics.median(data)
-stdev = statistics.stdev(data)
+mean = round(statistics.mean(data), 2)
+median = round(statistics.median(data), 2)
+stdev = round(statistics.stdev(data), 2)
+mode = statistics.mode(data)
+minimum = min(data)
+maximum = max(data)
+variance = round(statistics.variance(data), 2)
 
 # In kết quả
 print("Trung bình:", mean)
 print("Trung vị:", median)
 print("Độ lệch chuẩn:", stdev)
+print("Mode:", mode)
+print("Giá trị nhỏ nhất:", minimum)
+print("Giá trị lớn nhất:", maximum)
+print("Phương sai:", variance)
